2d67359d34e1b0af4ccf58345b2b4286498195ef
Anton Zekeriev <antonzekeriev@gmail.com>
2020-09-17 14:44:51 +0200

    Pagina temporal...

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="container">
-    <h1>Anton Zekeriev Rodin</h1>
-    <p>¡Hello!</p>
-  </div>
+  <section class="hero is-large bold">
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 class="title">Antón Zekeriev Rodin</h1>
+        <h2 class="subtitle">Actualmente la web se encuentra en construcción.</h2>
+        
+        <p class="is-medium block">
+          Puedes contactar conmigo a traves del correo <a href="mailto:azr@azr.es">azr@azr.es</a> o a través del telefono +34 675 649 322. Tambien me puedes encontrar aquí:
+        </p>
+
+        <p class="is-medium block">
+          <a href="https://github.com/antonrodin" class="button is-light">GitHub</a>
+          <a href="https://www.youtube.com/c/AZRMedia" class="button is-light">Youtube</a>
+          <a href="https://www.linkedin.com/in/azrodin/" class="button is-light">LinkEdin</a>
+          <a href="https://www.azrodin.com/" class="button is-light">Blog</a>
+        </p>
+        
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-  
-}
+export default {};
 </script>
